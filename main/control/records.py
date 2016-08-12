c8a29fb9ba75695f56974026c40c65bc361d8d78
tvancann <tvancann@ebay.com>
2016-08-12 09:40:57 +0200

    Default activities

--- main/control/records.py
+++ main/control/records.py
@@ -1,7 +1,12 @@
 import wtforms
+from flask import make_response, render_template, Response
 from flask_wtf import Form
 
-from model import Category
+from flask_restful import Resource
+
+from auth import auth
+from main import api
+from model import BaseCategory, BaseMetric, BaseActivity
 
 
 class RecordForm(Form):
@@ -13,6 +18,102 @@ class RecordForm(Form):
 
 	def __init__(self, *args, **kwargs):
 		super(RecordForm, self).__init__(*args, **kwargs)
-		cats, _, _ = Category.get_dbs()
-		print(cats)
+		cats, _ = BaseCategory.get_dbs()
+		print cats
 		self.category.choices = [(cat.key.urlsafe(), cat.name) for cat in cats]
+
+
+class Record(Resource):
+	def get(self):
+		return make_response(render_template("records/track_new.html", form=RecordForm()))
+
+
+class InitActivities(Resource):
+	@staticmethod
+	def get():
+		crossfit = BaseCategory(name="Crossfit").put()
+		running = BaseCategory(name="Running").put()
+		lifting = BaseCategory(name="Weight Lifting").put()
+
+		dist = BaseMetric(name="Distance").put()
+		time = BaseMetric(name="Time").put()
+		amrap = BaseMetric(name="AMRAP").put()
+		rep = BaseMetric(name="One-Rep Max").put()
+
+		BaseActivity(category_key=running, metric_key=time, name="400m").put()
+		BaseActivity(category_key=running, metric_key=time, name="1k").put()
+		BaseActivity(category_key=running, metric_key=time, name="1mi").put()
+		BaseActivity(category_key=running, metric_key=time, name="5k").put()
+		BaseActivity(category_key=running, metric_key=time, name="10k").put()
+		BaseActivity(category_key=running, metric_key=time, name="half marathon").put()
+		BaseActivity(category_key=running, metric_key=time, name="marathon").put()
+		BaseActivity(category_key=running, metric_key=dist, name="Longest run").put()
+
+		BaseActivity(category_key=lifting, metric_key=rep, name="Deadlift").put()
+		BaseActivity(category_key=lifting, metric_key=rep, name="Squat").put()
+		BaseActivity(category_key=lifting, metric_key=rep, name="Bench Press").put()
+		BaseActivity(category_key=lifting, metric_key=rep, name="Shoulder Press").put()
+		BaseActivity(category_key=lifting, metric_key=rep, name="Push Jerk").put()
+		BaseActivity(category_key=lifting, metric_key=rep, name="Power Clean").put()
+		BaseActivity(category_key=lifting, metric_key=rep, name="Squat Clean").put()
+		BaseActivity(category_key=lifting, metric_key=rep, name="Power Snatch").put()
+
+		BaseActivity(category_key=crossfit, metric_key=time, name="Grace", description=grace).put()
+		BaseActivity(category_key=crossfit, metric_key=time, name="Fran", description=fran).put()
+		BaseActivity(category_key=crossfit, metric_key=time, name="Helen", description=helen).put()
+		BaseActivity(category_key=crossfit, metric_key=time, name="Filthy 50", description=filthy).put()
+		BaseActivity(category_key=crossfit, metric_key=amrap, name="Fight Gone Bad", description=fgb).put()
+		BaseActivity(category_key=crossfit, metric_key=time, name="DT", description=dt).put()
+		return Response(status=204)
+
+
+api.add_resource(Record, '/track_new')
+api.add_resource(InitActivities, '/init')
+
+grace = """
+30 power clean and push jerks
+"""
+
+fran = """
+21-15-9 reps of:
+95lbs/43kg Thrusters
+Pull-ups
+"""
+
+dt = """
+Five rounds of:
+155lbs/70kg Deadlift, 12 reps
+155lbs/70kg  Hang power clean, 9 reps
+155lbs/70kg Push jerk, 6 reps
+"""
+
+helen = """
+Three rounds of:
+Run 400 meters
+1.5pood/55lbs/25kg Kettlebell X 21 swings
+12 Pull-ups
+"""
+
+filthy = """
+50 Box jump, 24"/61cm box
+50 Jumping pull-ups
+50 Kettlebell swings, 1 pood/36lbs/16kg
+Walking Lunges, 50 steps
+50 Knees to elbows
+50 Push press, 45lbs/21kg
+50 Back extensions
+50 Wall ball shots, 20lbs/9kg ball
+50 Burpees
+50 Double unders
+"""
+
+fgb = """
+Three rounds of:
+Wall-ball, 20lbs/9kg ball, 10ft/3m target (Reps)
+Sumo deadlift high-pull, 75lbs/34kg (Reps)
+Box Jump, 20"/51cm box (Reps)
+Push-press, 75lbs/34kg (Reps)
+Row (Calories)
+
+In this workout you move from each of five stations after a minute.The clock does not reset or stop between exercises. This is a five-minute round from which a one-minute break is allowed before repeating. On call of "rotate", the athletes must move to next station immediately for best score. One point is given for each rep, except on the rower where each calorie is one point.
+"""
--- main/model/records.py
+++ main/model/records.py
@@ -8,30 +8,30 @@ class BaseUser(model.Base):
 	user_key = ndb.KeyProperty(kind=model.User, required=True)
 
 
-class Category(BaseUser):
+class BaseCategory(model.Base, polymodel.PolyModel):
 	name = ndb.StringProperty(required=True)
 
 
-class Metric(BaseUser):
+class BaseMetric(model.Base, polymodel.PolyModel):
 	name = ndb.StringProperty(required=True)
 
 
-class BaseActivity(BaseUser, polymodel.PolyModel):
-	category_key = ndb.KeyProperty(kind=Category, required=True)
-	metric_key = ndb.KeyProperty(kind=Metric, required=True)
+class BaseActivity(model.Base, polymodel.PolyModel):
+	category_key = ndb.KeyProperty(kind=BaseCategory, required=True)
+	metric_key = ndb.KeyProperty(kind=BaseMetric, required=True)
 
 	name = ndb.StringProperty()
 	description = ndb.StringProperty()
 
 
-class CrossFit(BaseActivity):
-	rxd = ndb.BooleanProperty()
-
-
-class Record(BaseUser):
+class BaseRecord(BaseUser, polymodel.PolyModel):
 	activity = ndb.KeyProperty(kind=BaseActivity, required=True)
-	category = ndb.KeyProperty(kind=Category, required=True)
+	category = ndb.KeyProperty(kind=BaseCategory, required=True)
 
 	value = ndb.StringProperty(required=True)
 	date = ndb.DateProperty()
 	notes = ndb.StringProperty()
+
+
+class CrossfitRecord(BaseRecord):
+	rxd = ndb.BooleanProperty()
